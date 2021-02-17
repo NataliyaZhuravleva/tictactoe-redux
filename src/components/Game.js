305c3f61d5e68cafdc4a010c740da33223780b81
Constantine <faustlarsen@gmail.com>
2021-02-16 19:41:46 -0700

    Fix clicking on step button
    Co-authored-by: Nataliya Zhuravleva<natalindria@gmail.com>

--- src/components/Game.js
+++ src/components/Game.js
@@ -15,8 +15,8 @@ class Game extends React.Component {
     };
   }
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber +1);
-    const current = history[this.state.stepNumber];
+    const history = this.state.history.slice(0, this.state.stepNumber+1);
+    const current = history[history.length-1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -36,16 +36,16 @@ class Game extends React.Component {
   jumpTo(step) {
     this.setState({
       stepNumber: step,
-      xIsNext: (step % 2) === 0,
+      xIsNext: step % 2 === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
-    const moves = history.map((step, move) => {
+    const moves = history.map((step, move) => { 
       const desc = move ? "Go to move #" + move : "Go to game start";
       return (
         <li key={move}>
